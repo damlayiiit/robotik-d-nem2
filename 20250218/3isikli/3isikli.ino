643cff546c1e2efc79952a535fbac3b934c1ddaa
damlayiiit <damlayigit10@icloud.com>
2025-02-18 14:34:37 +0300

    Add files via upload

---
+++ 20250218/3isikli/3isikli.ino
@@ -0,0 +1,30 @@
+void setup() {
+pinMode(A0,INPUT);
+pinMode(3,OUTPUT); 
+pinMode(4.OUTPUT);
+pinMode(5,OUTPUT);
+Serial.begin(9600);
+}
+
+
+void loop() {
+int LDR=analongRead(A0);
+Serial.print("A0<GİRİŞ)=");
+Serial.print(LDR);
+if(LDR<30){
+  digitalWrite(3,1);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+}
+else if(LDR<65){
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(5,0);
+}
+else{
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(5,1);
+}
+
+}
